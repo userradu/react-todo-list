b55d8d5720e8761a29b85a1157686172a5fb5f94
radu <userradu91@gmail.com>
2019-08-07 22:46:16 +0300

    Add tests for the StatusFilter component

---
+++ src/status-filter/StatusFilter.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import StatusFilter from './StatusFilter';
+
+describe('StatusFilter', () => {
+
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <StatusFilter
+        className="customClass"
+        currentStatus="All"
+        status="All"
+        onStatusFilterChanged={() => { }} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('calls prop.onStatusFilterChanged() with prop.status as argument', () => {
+    const onStatusFilterChanged = sinon.spy();
+    const status = 'All';
+
+    const wrapper = shallow(<StatusFilter
+      className="customClass"
+      currentStatus="All"
+      status={status}
+      onStatusFilterChanged={onStatusFilterChanged} />);
+
+    wrapper.find('button').simulate('click');
+
+    expect(onStatusFilterChanged.calledWith(status)).toBe(true);
+  });
+
+  it("it doesn't add the 'selected' class if the current status is equal to its own status", () => {
+    const wrapper = shallow(<StatusFilter
+      className="customClass"
+      currentStatus="All"
+      status="Complete"
+      onStatusFilterChanged={() => { }} />);
+
+    expect(wrapper.find('button').hasClass('selected')).toBe(false);
+  });
+
+  it("it adds the 'selected' class if the current status is equal to its own status", () => {
+    const wrapper = shallow(<StatusFilter
+      className="customClass"
+      currentStatus="All"
+      status="All"
+      onStatusFilterChanged={() => { }} />);
+
+    expect(wrapper.find('button').hasClass('selected')).toBe(true);
+  });
+
+});
